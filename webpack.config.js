b48b06eeca41adad916bd265b3f1bf7be8fee6b5
MrLuckyCat <joewilliamdd31@gmail.com>
2023-01-19 21:10:38 +0100

    Add css loaders to use later

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,14 @@ module.exports = {
       filename: "main.js",
       path: path.resolve(__dirname, "dist"),
    },
+   module: {
+      rules: [
+         {
+            test: /\.css$/i,
+            use: "css-loader",
+         },
+      ],
+   },
+   devtools: "inline-source-map",
    mode: "development",
 };
