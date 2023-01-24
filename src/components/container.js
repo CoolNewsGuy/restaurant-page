e0e60255b7f574804c52f76bc3376591ea438a47
MrLuckyCat <joewilliamdd31@gmail.com>
2023-01-24 19:24:07 +0100

    Add a container for home, menu and about

---
+++ src/components/container.js
@@ -0,0 +1,10 @@
+import "../css/container.css";
+
+function Container() {
+   let container = document.createElement("div");
+
+   container.setAttribute("class", "container");
+   return container;
+}
+
+export default Container;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import "./assets/TangerineRegular.ttf";
 import "./css/style.css";
 import Navbar from "./components/navbar";
 import Home from "./components/home";
+import Container from "./components/container";
 
 let pageContent = document.getElementById("content");
+let sectionsContainer = Container();
 
-pageContent.append(Navbar(), Home());
+sectionsContainer.append(Home());
+pageContent.append(Navbar(), sectionsContainer);
