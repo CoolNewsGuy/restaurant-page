13ba52130b3ae355d76b96492ee4e766d1144ffc
MrLuckyCat <joewilliamdd31@gmail.com>
2023-01-27 08:34:11 +0100

    Convert contact section to a javascript component

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import Navbar from "./components/navbar";
 import Container from "./components/container";
 import Home from "./components/home";
 import Menu from "./components/menu";
+import Contact from "./components/contact";
 
 let pageContent = document.getElementById("content");
 let sectionsContainer = Container();
 
-sectionsContainer.append(Home(), Menu());
-pageContent.append(Navbar());
+sectionsContainer.append(Home(), Menu(), Contact());
+pageContent.append(Navbar(), sectionsContainer);
